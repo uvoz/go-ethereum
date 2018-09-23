7607c391b1333296afe35826cbd55b010c3920be
uvoz <bob.heida@gmail.com>
2018-09-23 13:55:22 +0200

    bootnodes

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -19,29 +19,20 @@ package params
 // MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
 // the main Ethereum network.
 var MainnetBootnodes = []string{
-	// Ethereum Foundation Go Bootnodes
-	"enode://05b06f88d6236f0518a7f954519997fc9ace0b5edfb8edfdc6a0d8f203b84974fb3ba9c38eb251839446093dfa5760707cbc4e71be0f9e2acb24771c570a4c5a@3.1.4.1:30301" // //MODIFIED for test
-	// Ethereum Foundation C++ Bootnodes
-	//"enode://979b7fa28feeb35a4741660a16076f1943202cb72b6af70d327f053e248bab9ba81760f39d0701ef1d8f89cc1fbd2cacba0710a12cd5314d5e0c9021aa3637f9@5.1.83.226:30303", // DE
-}
+	// Ethereum Foundation Go Bootnodes MODIFIED for test
+	"enode://05b06f88d6236f0518a7f954519997fc9ace0b5edfb8edfdc6a0d8f203b84974fb3ba9c38eb251839446093dfa5760707cbc4e71be0f9e2acb24771c570a4c5a@3.1.4.1:30301"}
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
-// Ropsten test network.
+// Ropsten test network. // //MODIFIED for test
 var TestnetBootnodes = []string{
-	"enode://05b06f88d6236f0518a7f954519997fc9ace0b5edfb8edfdc6a0d8f203b84974fb3ba9c38eb251839446093dfa5760707cbc4e71be0f9e2acb24771c570a4c5a@3.1.4.1:30301" // //MODIFIED for test
-	
-}
+	"enode://05b06f88d6236f0518a7f954519997fc9ace0b5edfb8edfdc6a0d8f203b84974fb3ba9c38eb251839446093dfa5760707cbc4e71be0f9e2acb24771c570a4c5a@3.1.4.1:30301"}
 
 // RinkebyBootnodes are the enode URLs of the P2P bootstrap nodes running on the
 // Rinkeby test network.
 var RinkebyBootnodes = []string{
-	"enode://05b06f88d6236f0518a7f954519997fc9ace0b5edfb8edfdc6a0d8f203b84974fb3ba9c38eb251839446093dfa5760707cbc4e71be0f9e2acb24771c570a4c5a@3.1.4.1:30301" // //MODIFIED for test
-	
-}
+	"enode://05b06f88d6236f0518a7f954519997fc9ace0b5edfb8edfdc6a0d8f203b84974fb3ba9c38eb251839446093dfa5760707cbc4e71be0f9e2acb24771c570a4c5a@3.1.4.1:30301"}
 
 // DiscoveryV5Bootnodes are the enode URLs of the P2P bootstrap nodes for the
 // experimental RLPx v5 topic-discovery network.
 var DiscoveryV5Bootnodes = []string{
-	"enode://05b06f88d6236f0518a7f954519997fc9ace0b5edfb8edfdc6a0d8f203b84974fb3ba9c38eb251839446093dfa5760707cbc4e71be0f9e2acb24771c570a4c5a@3.1.4.1:30301" // //MODIFIED for test
-	
-}
+	"enode://05b06f88d6236f0518a7f954519997fc9ace0b5edfb8edfdc6a0d8f203b84974fb3ba9c38eb251839446093dfa5760707cbc4e71be0f9e2acb24771c570a4c5a@3.1.4.1:30301"}
